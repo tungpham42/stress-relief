ee7e4929a7e227fa98a33f049722d1f68f9a0dae
Tung Pham <tung.42@gmail.com>
2025-06-24 07:57:00 +0700

    logo brand

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import { faLeaf } from "@fortawesome/free-solid-svg-icons";
 import MusicPlayer from "./components/MusicPlayer";
 import Quotes from "./components/Quotes";
 import MeditationTimer from "./components/MeditationTimer";
+import MainBrandLogo from "./components/MainBrandLogo";
 
 function App() {
   return (
-    <Container className="my-4">
-      <h1 className="text-center">
-        <FontAwesomeIcon icon={faLeaf} className="me-2" />
-        Ứng dụng giảm căng thẳng
-      </h1>
-      <MusicPlayer />
-      <Quotes />
-      <MeditationTimer />
-    </Container>
+    <>
+      <MainBrandLogo
+        logoSrc="/soft-logo.webp"
+        mainDomain="soft.io.vn"
+        dismissible={false}
+        altText="Logo Soft"
+      />
+      <Container className="my-4">
+        <h1 className="text-center">
+          <FontAwesomeIcon icon={faLeaf} className="me-2" />
+          Ứng dụng giảm căng thẳng
+        </h1>
+        <MusicPlayer />
+        <Quotes />
+        <MeditationTimer />
+      </Container>
+    </>
   );
 }
 
